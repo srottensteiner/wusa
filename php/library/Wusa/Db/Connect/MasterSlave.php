7bc4743adc0efd89e07ce76f7ab77868dc798692
Lukas Plattner <lukas01@gmail.com>
2012-10-23 01:00:00 +0200

    Prepared for Master/Slave Connections

---
+++ php/library/Wusa/Db/Connect/MasterSlave.php
@@ -0,0 +1,12 @@
+<?php
+namespace Wusa\Db\Connect;
+/**
+ * @author Lukas Plattner
+ */
+class MasterSlave implements InterfaceConnect
+{
+    public function connect($config, $type, $serverName)
+    {
+
+    }
+}
